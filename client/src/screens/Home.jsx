584acff5d837246728bcd9e8d654dd6b3c129b5c
Hithy <hithyisgod@gmail.com>
2023-12-01 03:44:35 +0200

    Added all the new components I made today
    Added audio player bar
    
    Added sidebar upload modal interactivity and the modal itself
    
    Added the active playlist component

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Sidebar from "../components/Sidebar";
 import AudioPlayerBar from "../components/AudioPlayerBar";
+import ActivePlaylist from "../components/ActivePlaylist";
+import PlaylistContext from "../providers/PlaylistContext";
+import UploadModal from "../components/UploadModal";
 
 const Home = () => {
+	const [showUploadModal, setShowUploadModal] = useState(false);
+	let playlists = ['Weeknd'];
+
 	return (
-		<div className='min-h-screen max-h-screen min-w-max flex flex-col bg-black'>
-			<div className='flex flex-col flex-grow px-5 pt-5'>
-				<Sidebar />
-			</div>
-			<AudioPlayerBar />
-		</div>
+		<>
+			{ showUploadModal &&
+				<UploadModal
+					setShowUploadModal={setShowUploadModal}
+				/>
+			}
+			<PlaylistContext.Provider value={playlists}>
+				<div className='max-w-screen max-h-screen h-screen flex-col bg-black overflow-x-hidden'>
+					<div className='flex space-x-5 pt-5 px-5 h-[87%]'>
+						<Sidebar
+							showUploadModal={showUploadModal}
+							setShowUploadModal={setShowUploadModal}
+						/>
+						<ActivePlaylist />
+					</div>
+					<AudioPlayerBar />
+				</div>
+			</PlaylistContext.Provider>
+		</>
 	);
 };
 
